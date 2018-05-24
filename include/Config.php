893ca96a31f2a5954441fdebac3b31cf6cecec3e
root <root@localhost.localdomain>
2018-05-24 06:58:52 +0000

    removed the automatic deployment script and changed the configurations

--- include/Config.php
+++ include/Config.php
@@ -3,8 +3,8 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'nyagaka2013');
-define('DB_HOST', 'localhost');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', '45.79.180.248');
 define('DB_NAME', 'chores');
 
 define('USER_CREATED_SUCCESSFULLY', 0);
